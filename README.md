bb4d5254752fff7961fc784ec10187b06025b096
mukesh1327 <mugicoder13@gmail.com>
2025-04-01 18:16:24 +0530

    Created README file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python fastAPI hello world
+
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+
+Create .env with properties below
+APP_TITLE="Value Point Systems Demo"
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = FastAPI()
 
-# Load the title from an environment variable (similar to application.properties)
-APP_TITLE = os.getenv("APP_TITLE", "Value Point Systems Demo - Openshift CI CD")
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
+# Define a static image path
+IMAGE_URL = "/images/noventiq-vp.png"  # Make sure this path matches the actual file location
+
+# Read APP_TITLE from system env or .env file (system env takes priority)
+APP_TITLE = os.getenv("APP_TITLE", "Default Title")
 
 @app.get("/hello")
 async def hello_world():
     return {"message": "Hello, World!"}
 
-@app.get("/vpsdemo/{image_name}", response_class=HTMLResponse)
-async def get_image_html(image_name: str):
-    image_url = f"/images/{image_name}"
+@app.get("/vpsdemo", response_class=HTMLResponse)
+async def get_image_html():
     html_content = f"""
     <html>
         <head><title>Image Viewer</title></head>
         <body>
             <h1>{APP_TITLE}</h1>
             <h2>App modernization</h2>
-            <img src='{image_url}' alt='Image' width='500'/>
+            <img src='{IMAGE_URL}' alt='Image' width='500'/>
         </body>
     </html>
     """
